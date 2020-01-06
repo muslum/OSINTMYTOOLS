42c477a89c5b26361f38ea627a14152f7fff395d
Muslum Dag <cybersecurity@muslumdag.com>
2020-01-07 01:40:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 #	https://developer.twitter.com/en/apps
 #--------------------------------------------------------------------------------
 
-consumer_key = "33CkEKcxtgogJ3EH2INZmeGV3"
-consumer_secret = "FMjXk6B6wODmDKz96CQ3jgorwS873rzN2egLqsuZOWhOR5y7qM"
-access_key = "773656732841209856-peVV13jUA9Ab2iYfBWtLoNxAhEPq1Bi"
-access_secret = "K4dPkgToa9S7ooIZSkaU4ZC92oLeOCiWqi1OmOFk5Dtj3"
+consumer_key = "XXXXX"
+consumer_secret = "XXXXXX"
+access_key = "XXXXX"
+access_secret = "XXXXXXX"
